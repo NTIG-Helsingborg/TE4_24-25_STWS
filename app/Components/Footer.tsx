e5ed8144edd5409710b7c28e284dd93a24fc7e31
Nexusiusen <alexander.wikblad@elev.ga.ntig.se>
2024-10-11 13:43:48 +0200

    " Finished upp footer so it looks like the sketch made in figma"

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,13 +1,29 @@
 import React from "react";
+import Circle from "./Circle";
 
 const Footer = () => {
   return (
-    <div className="p-9 bg-gray-400 text-white content-center">
-      <div className="text-center">Footer</div>
-      <div>
-        <div className="rounded-full bg-white">a</div>
+    <>
+      <div className="p-9 bg-gray-400 text-white content-center fustat">
+        <div className="flex space-x-4 justify-center">
+          <Circle text=""></Circle>
+          <Circle text=""></Circle>
+          <Circle text=""></Circle>
+          <Circle text=""></Circle>
+          <Circle text=""></Circle>
+        </div>
+        <div className="flex space-x-7 justify-center mt-5">
+          <span className="">Home</span>
+          <span className="">Text</span>
+          <span className="">About</span>
+          <span className="">Contact us</span>
+          <span className="">Our team</span>
+        </div>
       </div>
-    </div>
+      <div className="w-screen text-center fustat p-2 bg-gray-500">
+        <span className="">Copyright ©2024; Designed by Nexusiusen</span>
+      </div>
+    </>
   );
 };
 
