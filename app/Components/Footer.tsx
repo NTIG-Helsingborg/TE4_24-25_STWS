409e9ca69b463363099eb355d132e5678073dca3
Nexusiusen <alexander.wikblad@elev.ga.ntig.se>
2024-10-11 10:45:30 +0200

    Created basic components

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="p-9 bg-gray-400 text-white content-center">
+      <div className="text-center">Footer</div>
+      <div>
+        <div className="rounded-full bg-white">a</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Footer from "./Components/Footer";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Footer />
+    </main>
+  );
 }
